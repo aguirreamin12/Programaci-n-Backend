028639bc91a4cd2d101053e8a92cf436b88262d5
Santino Aguirre Amin <santinoaguirre2004@gmail.com>
2023-04-27 19:32:27 -0300

    Clase 46

--- src/graphql/Schema.js
+++ src/graphql/Schema.js
@@ -1,19 +1,20 @@
 import {buildSchema} from "graphql";
 import {ProductoType} from "./types/Producto.type.js";
 import {CarritoType} from "./types/Carrito.type.js";
-import {GetAllCarritosQuery} from "./queries/GetAllCarritos.query.js";
-import {GetAllProductosQuery} from "./queries/GetAllProductos.query.js";
-import {CreateCarritoMutation} from "./mutations/CreateCarrito.mutation.js";
-import {DeleteCarritoByIdMutation} from "./mutations/DeleteCarritoById.mutation.js";
-import {GetAllProductsFromCartByIdQuery} from "./queries/GetAllProductsFromCartById.query.js";
-import {SaveProductToCartMutation} from "./mutations/SaveProductToCart.mutation.js";
-import {DeleteProductFromCartMutation} from "./mutations/DeleteProductFromCart.mutation.js";
-import {GetProductByIdQuery} from "./queries/GetProductById.query.js";
 import {ProductoNewInput} from "./inputs/ProductoNew.input.js";
-import {CreateProductoMutation} from "./mutations/CreateProducto.mutation.js";
 import {ProductoUpdateInput} from "./inputs/ProductoUpdate.input.js";
-import {UpdateProductByIdMutation} from "./mutations/UpdateProductById.mutation.js";
-import {DeleteProductByIdMutation} from "./mutations/DeleteProductById.mutation.js";
+/** Queries and mutations **/
+import {GetAllCarritosQuery, getAllCarritos} from "./queries/GetAllCarritos.query.js";
+import {GetAllProductosQuery, getAllProductos} from "./queries/GetAllProductos.query.js";
+import {CreateCarritoMutation, createCarrito} from "./mutations/CreateCarrito.mutation.js";
+import {DeleteCarritoByIdMutation, deleteCarritoById} from "./mutations/DeleteCarritoById.mutation.js";
+import {GetAllProductsFromCartByIdQuery, getAllProductsFromCartById} from "./queries/GetAllProductsFromCartById.query.js";
+import {SaveProductToCartMutation, saveProductToCart} from "./mutations/SaveProductToCart.mutation.js";
+import {DeleteProductFromCartMutation, deleteProductFromCart} from "./mutations/DeleteProductFromCart.mutation.js";
+import {GetProductByIdQuery, getProductById} from "./queries/GetProductById.query.js";
+import {CreateProductoMutation, createProduct} from "./mutations/CreateProducto.mutation.js";
+import {UpdateProductByIdMutation, updateProductById} from "./mutations/UpdateProductById.mutation.js";
+import {DeleteProductByIdMutation, deleteProductById} from "./mutations/DeleteProductById.mutation.js";
 
 export const schema = buildSchema(`
   ${ProductoType}
@@ -38,3 +39,17 @@ export const schema = buildSchema(`
     ${DeleteProductByIdMutation}
   }
 `);
+
+export const queriesAndMutations = {
+    getAllCarritos,
+    getAllProductos,
+    createCarrito,
+    deleteCarritoById,
+    getAllProductsFromCartById,
+    saveProductToCart,
+    deleteProductFromCart,
+    getProductById,
+    createProduct,
+    updateProductById,
+    deleteProductById
+}
